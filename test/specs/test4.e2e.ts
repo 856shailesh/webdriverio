527d090fdb3b7d16c1a79d39d8d009154b1a58e2
shaileshkumar <856shailesh@gmail.com>
2025-02-08 17:10:56 +0530

    web tables start

--- test/specs/test4.e2e.ts
+++ test/specs/test4.e2e.ts
@@ -19,9 +19,32 @@ describe("day10", () => {
         const timeouts1 = await browser.getTimeouts();
         console.log("After Timeouts are : ", timeouts1);
     })
-    it("Day 11 waiting strategy", async () => {
+    xit("Day 11 waiting strategy", async () => {
         await browser.url("xpath-practice-page/");
         const timeouts = await browser.getTimeouts();
         console.log(" Timeouts are : ", timeouts);
+    })
+    it("Day 12 implement waits", async () => {
+        await browser.url("/account/login");
+        await browser.maximizeWindow();
+        await $("input[name=email]").setValue("856shaileshk@gmail.com")
+        await $("input[name=password]").setValue("123456")
+        await $("button[type=submit]").click();
+        //Home 
+        await $("//a/span[text()='Nike react phantom run flyknit 2']").click();
+        //Product
+        const productName = await $("h1.product-single-name").getText();
+        console.log("Product name is ", productName)
+        //click not happening here , need to wait
+        await $("//a[text()='X']").click();
+        await browser.waitUntil(async () => {
+            let classvalue = await $("//a[text()='X']/parent::li").getAttribute("class");
+            if (classvalue === 'selected12') return true;
+            else return false;
+        }, { timeout: 5000, timeoutMsg: "Didn't attained selected value" });
+        await $("//a[text()='Green']").click();
+        await $("input[name = 'qty']").setValue(3);
+        await $("button[type = 'button'] span").click();
+        console.log("End of Script");
     })
 })
